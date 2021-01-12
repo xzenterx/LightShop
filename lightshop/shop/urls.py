8a9377334c9dcb46194740b465fe32890718b83e
xzenterx <xzenterx7755@gmail.com>
2021-01-12 23:22:42 +0200

    Added home page

--- lightshop/shop/urls.py
+++ lightshop/shop/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.product_list, name='product_list'),
+    url(r'^$', views.product_home, name='product_home'),
     url(r'^(?P<category_slug>[-\w]+)/$',
         views.product_list,
         name='product_list_by_category'),
--- lightshop/shop/views.py
+++ lightshop/shop/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 
+def product_home(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/home.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
